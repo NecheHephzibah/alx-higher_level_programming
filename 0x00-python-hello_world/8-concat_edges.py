919e46944ba2e0c43b537dc0e3561d65c8bf847c
NecheHephzibah <neche.bless@gmail.com>
2024-01-04 01:02:12 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + chr(32) + str[107:111] + chr(32) + "Python"
+str = str[39:66] + chr(32) + str[107:111] + chr(32) + chr(80) + chr(121) + chr(116) + chr(104) + chr(111) + chr(110)
 print(str)
