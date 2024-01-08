053a7251aa0dcd787880cfd1ab4065806787dd17
NecheHephzibah <neche.bless@gmail.com>
2024-01-08 12:53:41 -0800

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,13 +4,8 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix or not all(matrix):
         return
 
-    num_row = len(matrix)
-    num_col = len(matrix[0])
-    trans_matrix = []
-    for i in range (num_col):
-        trans_matrix.append([row[i] for row in matrix])
+    for row in matrix:
+        for i in range(len(row)):
 
-    for row in trans_matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+            print("{:d}".format(row[i]), end=" ")
         print()
