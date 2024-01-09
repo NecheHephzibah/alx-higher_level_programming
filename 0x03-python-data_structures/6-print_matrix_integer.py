bca846107dbe6b71be29069acb5d7c13c5b7b5f3
NecheHephzibah <neche.bless@gmail.com>
2024-01-09 06:25:21 -0800

    List os lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
             for inner_row in row:
-                print("{:d}".format(inner_row), end=" " if inner_row != row[-1] else "")
+                print("{:d}".format(inner_row), end=" "
+                      if inner_row != row[-1] else "")
             print()
