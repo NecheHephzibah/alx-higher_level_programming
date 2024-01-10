963bd4499cb42a5f04674c74e44187aba3b20fd5
NecheHephzibah <neche.bless@gmail.com>
2024-01-10 11:50:04 -0800

    Who  are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import  hidden_4
+    import hidden_4
 
     sort_list = dir(hidden_4)
     my_list = [x for x in sort_list if not x.startswith('__')]
