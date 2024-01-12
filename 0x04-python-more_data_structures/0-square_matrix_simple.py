9290e52e0891d68b845f49e0cbce8827244d0dcb
NecheHephzibah <neche.bless@gmail.com>
2024-01-11 16:21:02 -0800

    matrix using map and lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=None):
+def square_matrix_simple(matrix=[]):
     if matrix:
         new_matrix = matrix.copy()
-        for row in matrix:
+        for row in range(len(matrix)):
             new_matrix[row] = list(map(lambda x: x**2, matrix[row]))
     return (new_matrix)
