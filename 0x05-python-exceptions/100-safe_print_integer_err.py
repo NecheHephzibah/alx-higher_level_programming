ea2df0593a180d746028b269131680db1bc23dc3
NecheHephzibah <neche.bless@gmail.com>
2024-01-24 04:17:41 -0800

    Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,13 +7,8 @@ def safe_print_integer_err(value):
     try:
         if value is None:
             raise TypeError("NoneType is not an integer")
-        formatted_value = "{:d}".format(value)
-        print(formatted_value)
+        print("{:d}".format(value))
         return True
-    except ValueError:
-        print("Exception: Cannot format float as integer",
-              file=sys.stderr)
-        return False
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
