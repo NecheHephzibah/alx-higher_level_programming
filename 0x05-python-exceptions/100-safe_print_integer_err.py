b7ee9ea693a7f70836e7c3d6194f1676afc69643
NecheHephzibah <neche.bless@gmail.com>
2024-01-24 04:02:08 -0800

    Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,10 @@ import sys
 
 def safe_print_integer_err(value):
     try:
+        if value is None:
+            raise TypeError("NoneType is not an integer")
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("{}".format(e), file=sys.stderr)
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
