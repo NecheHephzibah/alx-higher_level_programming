90cfd9fe5ebd41e87029d4b2a80a0629dd4391e0
NecheHephzibah <neche.bless@gmail.com>
2024-01-29 13:05:05 -0800

    Coordinates oof a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,9 +42,8 @@ class Square:
                 len(value) != 2 or
                 not all(isinstance(number, int) for number in value) or
                 not all(number >= 0 for number in value)):
-            raise TypeError("position must be a tuple
-                            of two positive integers")
-            self.__position = value
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Returns the area of a square."""
