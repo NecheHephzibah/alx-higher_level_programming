dec175bc85259b795820a977b48a7d3d455f1603
NecheHephzibah <neche.bless@gmail.com>
2024-02-06 13:53:43 -0800

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module contains a function that prints strings."""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Defines a function that receives two strings and prints them out.
+
+    Args:
+        first_name (str): The first argument to the function.
+        last_name (str): The second argumnet to the function.
+
+    Returns:
+        Prints an output with the arguments.
+
+    Raises:
+        TypeError: If first_name is not a string.
+        TypeError: If last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile(tests/3-say_my_name.txt)
