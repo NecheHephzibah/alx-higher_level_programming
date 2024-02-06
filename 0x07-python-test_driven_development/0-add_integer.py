510092620b753dcf6b8028b80ae2eafdd0f9b918
NecheHephzibah <neche.bless@gmail.com>
2024-02-06 01:29:20 -0800

    Py doc

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,15 +6,17 @@ def add_integer(a, b=98):
     """
     Calculate the sum of a and b.
 
-    Parameters:
-    a (int or float): The first number.
-    b (int or float): The second number.
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number (default is 98).
 
     Returns:
-    int: The sum of a and b
+    int: The sum of `a` and `b`
 
     Raises:
     TypeError: If a or b is not an integer or float.
+    OverflowError: If infinity is provided as an argument.
+    ValueError: If NaN (Not-a-Number) is provided as an argument.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
