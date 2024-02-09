b67d4762d2562ce3c0ab64f8fd14ff7a7b3e74a6
NecheHephzibah <neche.bless@gmail.com>
2024-02-09 02:24:11 -0800

    Geometry module

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Represents a module containing a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines a class BaseGeometry"""
+
+    def area(self):
+        """Defines a public method instance"""
+        raise Exception("area() is not implemented")
