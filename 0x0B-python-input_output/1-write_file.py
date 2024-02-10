23b73146e687a84624509ef624d2a11c9e89e880
NecheHephzibah <neche.bless@gmail.com>
2024-02-10 11:34:24 -0800

    Read file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Represents function that writes a sstring to a text file."""
+
+
+def write_file(filename="", text=""):
+    """
+    Defines the function that writes to a string
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+
+    Returns:
+        The number of characters written.
+    """
+
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(text) 
