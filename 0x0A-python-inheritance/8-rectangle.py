aa5dbbf602dcce87cd232851a44df5cedea8984d
NecheHephzibah <neche.bless@gmail.com>
2024-02-10 04:15:28 -0800

    Debugging

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,32 @@
 #!/usr/bin/python3
 
 """Represents a module containing a class BaseGeometry"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+#!/usr/bin/python3
+
+"""Represents a module containing a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines a class BaseGeometry"""
+
+    def area(self):
+        """Defines a public method instance"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
