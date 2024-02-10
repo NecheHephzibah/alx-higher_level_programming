e1d328d34cced5391fb3180c64d4ed6f51a1e03e
NecheHephzibah <neche.bless@gmail.com>
2024-02-10 03:59:58 -0800

    Debugging

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,6 +6,8 @@
 class BaseGeometry:
     """Defines a class BaseGeometry"""
 
+    def __init__(self)
+
     def area(self):
         """Defines a public method instance"""
         raise Exception("area() is not implemented")
@@ -20,6 +22,9 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
+        self.name = name
+        self.value = value
+
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
