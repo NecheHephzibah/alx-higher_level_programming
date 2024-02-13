46c5755531686ac1ef78ce2da48b284a6a015c4b
NecheHephzibah <neche.bless@gmail.com>
2024-02-13 03:34:59 -0800

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,15 +4,15 @@
 
 class Base:
     """Defines a class Base"""
-        
+
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """Defines the class constructor"""
         if id is not None:
             self.id = id
         else:
-           Base. __nb_objects += 1
-           self.id = Base.__nb_objects
+            Base. __nb_objects += 1
+            self.id = Base.__nb_objects
 
         return
