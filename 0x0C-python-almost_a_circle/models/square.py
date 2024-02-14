ed67b017e70935983d0a3a95a8af7b1e79aab913
NecheHephzibah <neche.bless@gmail.com>
2024-02-14 00:56:18 -0800

    Display #0

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,7 +21,7 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        """Gets size of the Square."""
+        """Gets and sets size of the Square."""
         return self.width
 
     @size.setter
@@ -31,7 +31,7 @@ class Square(Rectangle):
         if value <= 0:
             raise ValueError("width must be > 0")
         self.width = value
-        self.height= value
+        self.height = value
 
     def __str__(self):
         """
@@ -43,23 +43,30 @@ class Square(Rectangle):
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
 
     def update(self, *args, **kwargs):
-       if len(args):
-           for i, arg in enumerate(args):
-               if i == 0:
-                   self.id = arg
-               if i == 1:
-                   self.width = arg
-               if i == 2:
-                   self.x = arg
-               if i == 3:
-                   self.y = arg
-       else:
-           if "id" in kwargs:
-               self.id = kwargs["id"]
-           elif "size" in kwargs:
-               self.width = kwargs["size"]
-           elif "x" in kwargs:
-               self.x = kwargs["x"]
-           elif "y" in kwargs:
-               self.y = kwargs["y"]   
+        """
+        Assigns an argument to each attribute.
 
+        Args:
+           *args (positional argument): assigns argument variables by index.
+           **kwargs (keyword argument): assigns argument by their keys.
+
+        """
+        if len(args):
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.width = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            elif "size" in kwargs:
+                self.width = kwargs["size"]
+            elif "x" in kwargs:
+                self.x = kwargs["x"]
+            elif "y" in kwargs:
+                self.y = kwargs["y"]
