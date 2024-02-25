dbbc619020914d9c1d6c8dd453b948116460d77a
NecheHephzibah <neche.bless@gmail.com>
2024-02-25 05:35:08 -0800

    Square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,57 +2,7 @@
 
 """Represents a module containing a class BaseGeometry"""
 
-
-class BaseGeometry:
-    """Defines a class BaseGeometry"""
-
-    def area(self):
-        """Defines a public method instance"""
-        return
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Defines a child class Rectangle that inherits from parent class"""
-
-    def __init__(self, width, height):
-        """Defines a child class Rectangle.
-
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-        """
-        super().integer_validator("width", width)
-        self.__width = width
-
-        super().integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """Defines a public method instance area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Defines the string representation of the object"""
-        string = "[" + str(self.__class__.__name__) + "]"
-
-        string += str(self.__width) + "/" + str(self.__height)
-
-        return string
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
