dc54bb141752526bc0e4272dfba5071bc72b557a
NecheHephzibah <neche.bless@gmail.com>
2024-04-09 09:39:46 +0000

    corrected task 2

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+if (process.argv[2] === undefined ||
+    Number.isNaN(Number(process.argv[2]))) {
   console .log('Missing size');
 } else {
   const x = parseInt(process.argv[2]);
