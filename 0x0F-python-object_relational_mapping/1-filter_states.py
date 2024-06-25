e6cdeba82556bc77182b2332d185e536803c1cd0
NecheHephzibah <neche.bless@gmail.com>
2024-06-25 06:02:37 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""Module that connects to a MySQL database and lists all states starting"""
+"""Lists states"""
 
-from sys import argv
 import MySQLdb
-
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset="utf8"
-    )
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER states.id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         if row[1].startswith("N"):
             print(row)
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
