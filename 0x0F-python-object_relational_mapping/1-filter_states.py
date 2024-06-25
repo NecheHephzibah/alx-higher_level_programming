08cd1432d3f2d1c61d4db1c2f50364b91bfdfc1d
NecheHephzibah <neche.bless@gmail.com>
2024-06-25 05:38:10 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,48 +1,24 @@
 #!/usr/bin/python3
-"""
-Module that connects to a MySQL database and lists all states starting
-with 'N' from the states table.
-"""
+"""Module that connects to a MySQL database and lists all states starting"""
 
-import sys
+from sys import argv
 import MySQLdb
 
-def main():
-    """
-    Main function that connects to the MySQL database, and lists all states
-    starting with 'N' from the states table, and prints them.
-    """
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
 
-    # Connect to the database
+if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
     )
 
-    # Create a cursor object
     cursor = db.cursor()
-
-    # Execute the SQL query
-    cursor.execute("SELECT id, name FROM states WHERE name
-                   LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all the rows
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cursor.fetchall()
-
-    # Print the rows
-    for row in rows:
+    if row[1].startswith("N"):
         print(row)
-
-    # Close the cursor and the connection
     cursor.close()
     db.close()
-
-if __name__ == "__main__":
-    main()
-
