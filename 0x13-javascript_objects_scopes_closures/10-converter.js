608081ea4e86a7e9a0ffdf31eef033ddb5c01df7
NecheHephzibah <neche.bless@gmail.com>
2024-07-23 14:58:58 -0700

    1-rectangle

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/*This script converts a nuber fro base 10 to another base passed as args*/
+/* converts a number from base 10 to another base passed as argument */
 exports.converter = function (base) {
   return function (number) {
     return number.toString(base);
