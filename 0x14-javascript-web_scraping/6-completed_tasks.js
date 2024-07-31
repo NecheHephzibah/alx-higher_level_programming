8599a38e10358b4e77c5b0ba8181b13a05ff1341
NecheHephzibah <neche.bless@gmail.com>
2024-07-31 09:01:04 -0700

    Right order

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const completedTask = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!completedTask[todo.userId]) {
+        completedTask[todo.userId] = 1;
+      } else {
+        completedTask[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(completedTask);
+});
